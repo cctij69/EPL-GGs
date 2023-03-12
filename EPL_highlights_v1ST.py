ea74724ba7d0b3c296cf545a776e697ccf162200
Kristian Rejek <th14reg@gmail.com>
2023-03-12 15:42:45 -0400

    changed method of grabbing links to RSS

--- EPL_highlights_v1ST.py
+++ EPL_highlights_v1ST.py
@@ -1,26 +1,20 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-from youtubesearchpython import *
 import datetime
-from colorama import Fore
 import streamlit as st
 import feedparser
+import re
 
 
 tableList = []
 hotList = []
 gamesAndLinks = {}
+listOfGames = []
+dateRange = 0
 
-
-
-def getGames2(tableList,dateRange):
-
-    listOfGames = []
-    #dateRange = 2
+def figureOutDate(tableList,dateRange):
+    
     today = datetime.date.today()
-
-
-
     target_dayofweek = 0  
     current_dayofweek = datetime.datetime.now().weekday() # Today
 
@@ -29,12 +23,16 @@ def getGames2(tableList,dateRange):
         # target is in the current week
         #endDate = datetime.datetime.now() - datetime.timedelta(current_dayofweek - target_dayofweek)
         endDate = datetime.datetime.now() - datetime.timedelta(weeks=1) + datetime.timedelta(target_dayofweek - current_dayofweek)
+        endDatePlusOne = endDate + datetime.timedelta(weeks=1)
 
     else: 
         # target is in the previous week
         endDate = datetime.datetime.now() - datetime.timedelta(weeks=1) + datetime.timedelta(target_dayofweek - current_dayofweek)
 
+
     last_monday = f"{endDate:%Y%m%d}"
+    last_mondayPlusOne = f"{endDatePlusOne:%Y%m%d}"
+
 
 
 
@@ -43,8 +41,16 @@ def getGames2(tableList,dateRange):
     if dateRange == 2:
         print ("Processing.....")
         url = "https://areyouwatchingthis.com/soccer/games?date={}".format(last_monday)
+        getGames(tableList,url,dateRange,today)
+        url = "https://areyouwatchingthis.com/soccer/games?date={}".format(last_mondayPlusOne)
+        getGames(tableList,url,dateRange,today)
+        return dateRange
+
     else:
         url = "https://areyouwatchingthis.com/soccer/games"
+        getGames(tableList,url,dateRange,today)
+
+def getGames(tableList,url,dateRange,today):
     page = urlopen(url)
     html = page.read().decode("utf-8")
     soup = BeautifulSoup(html, "html.parser")
@@ -92,10 +98,6 @@ def getGames2(tableList,dateRange):
             if item not in hotList:
                 hotList.append(item)
 
-
-
-    getRSSLinks(hotList)
-            
 def getPremTable():
 
    
@@ -109,47 +111,36 @@ def getPremTable():
     for entry in entries[7:]:
         tableList.append(entry['title'])
 
-def getYTLinks(hotList):
-    #location = int(input("0-CAN links 1-UK links \n"))
-    for team in hotList:
-        teamNames = team[0] + " vs " + team[1]
-        search = CustomSearch((teamNames) + " on fuboTV Canada", VideoUploadDateFilter.thisWeek, limit = 1)
-        for video in search.result()['result']:
-            title = video['title']
-            if team[0] and team[1] in title:
-                gamesAndLinks[video['title']] = video['link']
-            else:
-                pass
-
-
-
-    print ("\n***** List of Games to watch! *****n")
-    st.write("******** List of Games to watch! ********")
-    for key, value in gamesAndLinks.items():
-        print ("\033[0m{}: \033[1;32m{}".format(key, value))
-        test = "{}: {}".format(key, value)
-        st.write(test)
-    print ("\n")
-    #st.write("\n")
-
-def getRSSLinks(hotList):
+def getRSSLinks(hotList,dateRange):
     rss_url = "https://www.youtube.com/feeds/videos.xml?channel_id=UCD2lJITnvzflNhOqQckMpQg"
     feed = feedparser.parse(rss_url)
     
-    
+    listOfWeeks=[]
+    for entry in feed.entries[:15]:
+        string = entry.title
+        pattern = r"(?<=\|).*?(?=\|)"
+        match = max(re.findall(pattern, string), key=lambda x: int(re.sub(r'\D', '', x)))
+        listOfWeeks.append(match)
+    max_num = max(map(lambda x: int(re.findall('\d+', x)[0]), listOfWeeks))
     
     
     for team in hotList:
         for entry in feed.entries[:15]:
-            if team[0] and team[1] in entry.title:
-                #print(entry.title, entry.link)
-                gamesAndLinks[entry.title] = entry.link
+            if dateRange == 2:
+                if team[0] and team[1] in entry.title:
+                    gamesAndLinks[entry.title] = [entry.link]
+            else:
+                if team[0] and team[1] in entry.title:
+                    if str(max_num) in entry.title:
+                        gamesAndLinks[entry.title] = [entry.link]
+
+
+
 
     st.write("******** List of Games to watch! ********")
     for key, value in gamesAndLinks.items():
         test = "{}: {}".format(key, value)
         st.write(test)
-  
 
 
 
@@ -157,21 +148,25 @@ def getRSSLinks(hotList):
 
 getPremTable()
 
+
 if st.button("Today"):
     dateRange = 0
-    getGames2(tableList,dateRange)
+    figureOutDate(tableList,dateRange)
 
 if st.button("This week"):
     dateRange = 1
-    getGames2(tableList,dateRange)
+    figureOutDate(tableList,dateRange)
 
 
-if st.button("Last week"):
+if st.button("All recent"):
     dateRange = 2
-    getGames2(tableList,dateRange)
+    figureOutDate(tableList,dateRange)
     
 
-
+if not hotList:
+    print("No good games found!")
+else:
+    getRSSLinks(hotList,dateRange)
 
 
 
